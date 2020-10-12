526d96077734ee34e6958c0f0d1c55d189767b12
Tkay KIM <tkay@clo3d.com>
2020-10-12 17:41:04 +0900

    WIP: Refactor FitMap.js

--- src/lib/fitting/FitMap.js
+++ src/lib/fitting/FitMap.js
@@ -21,22 +21,22 @@ export default class FitMap {
 
     const listFitMap = rootMap.get("listFitMap");
     listFitMap.forEach((fm) => {
-      const arrMatMeshID = fm.get("listMatMeshID");
+      const listMatMeshID = fm.get("listMatMeshID");
       if (!fm.has("baFitmapVertexColor")) {
         console.warn("WARNING: baFitmapVertexColor not found.");
         return;
       }
 
-      const arrVertexValue = readByteArray(
+      const listVertexValue = readByteArray(
         "Float",
         fm.get("baFitmapVertexColor")
       );
-      arrMatMeshID.forEach((matMeshID) => {
-        this.mapVertexColor.set(matMeshID, arrVertexValue);
+      listMatMeshID.forEach((matMeshID) => {
+        this.mapVertexColor.set(matMeshID, listVertexValue);
       });
     });
 
-    this.createVertexColor();
+    this.assignVertexColor();
   }
 
   clear() {
@@ -49,97 +49,44 @@ export default class FitMap {
     return readMap(new DataView(loadedData), { Offset: 0 });
   }
 
-  createVertexColor() {
-    for (const entries of this.mapVertexColor) {
-      const matMeshID = entries[0];
-      const arrVertexColor = entries[1];
-
+  assignVertexColor() {
+    this.mapVertexColor.forEach((listVertexColor, matMeshID) => {
       const matMesh = this.mapMatMesh.get(matMeshID);
-      const arrRGBA = Float32Array.from(arrVertexColor);
-
       if (!matMesh) {
         console.warn("WARNING: MatMeshID(" + matMeshID + ") not found.");
-        // return;
-      } else {
-        const geometry = matMesh.geometry;
-        // NOTE: vFittingColor is used by shaders to render fit map.
-        geometry.addAttribute(
-          "vFittingColor",
-          new THREE.BufferAttribute(arrRGBA, 4)
-        );
+        return;
       }
-    }
+
+      const arrRGBA = Float32Array.from(listVertexColor);
+      const geometry = matMesh.geometry;
+
+      // NOTE: vFittingColor is used by shaders to render fit map.
+      geometry.addAttribute(
+        "vFittingColor",
+        new THREE.BufferAttribute(arrRGBA, 4)
+      );
+    });
   }
 
   setOpacity(bOpacity) {
     this.loopToSet("bUseFitMapOpacity", bOpacity);
   }
 
+  setVisible(bVisible) {
+    this.loopToSet("bUseFitMap", bVisible);
+  }
+
   loopToSet(key, bValue) {
     const iValue = bValue ? 1 : 0;
-    for (const entries of this.mapVertexColor) {
-      const matMeshID = entries[0];
-      const matMesh = this.mapMatMesh.get(matMeshID);
 
+    this.mapVertexColor.forEach((v, matMeshID) => {
+      const matMesh = this.mapMatMesh.get(matMeshID);
       if (!matMesh) {
         console.warn("WARNING: MatMeshID(" + matMeshID + ") not found.");
       } else {
         const uniforms = matMesh.material.uniforms;
         uniforms[key].value = iValue;
       }
-    }
-  }
-
-  setVisible(bVisible) {
-    this.loopToSet("bUseFitMap", bVisible);
-  }
-
-  // TODO: Delete the comment below after QA
-  /*
-  // Parse the information to render a fit map from mapGeometry of ZRest.
-  parseMapGeometry(mapGeometry) {
-    const fieldKey = "listChildrenTransformer3D";
-
-    const shouldRecursive = (element) => {
-      return (
-        element instanceof Map &&
-        element.has(fieldKey) &&
-        element.get(fieldKey) != null
-      );
-    };
-
-    if (shouldRecursive(mapGeometry)) {
-      this.parseMapGeometry(mapGeometry.get(fieldKey));
-    } else {
-      mapGeometry.forEach((element) => {
-        if (shouldRecursive(element)) {
-          this.parseMapGeometry(element);
-        }
-        const listMatShape = element.get("listMatShape");
-        if (listMatShape) {
-          this.extractFitMapDataFromMatShape(listMatShape);
-        }
-      });
-    }
-  }
-
-  extractFitMapDataFromMatShape(listMatShape) {
-    // NOTE: vertexValue consists of RGBA list. (E.g. RGBARGBARGBA....)
-    // TODO: Ask to change field Key Fitmap to FitMap
-    const fieldKey = "baFitmapVertexValue";
-
-    listMatShape.forEach((matShape) => {
-      if (matShape.has(fieldKey)) {
-        const listMeshID = matShape.get("listMatMeshIDOnIndexedMesh");
-        const baVertexColor = readByteArray("Float", matShape.get(fieldKey));
-
-        // NOTE: meshID is an array but might have only 1 element.
-        listMeshID.forEach((meshID) => {
-          const matMeshID = meshID.get("uiMatMeshID");
-          this.mapVertexColor.set(matMeshID, baVertexColor);
-        });
-      }
     });
   }
-   */
 }
